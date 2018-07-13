986903185fee110f4631b4231dc37b994df56b70
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-13 18:08:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,10 +5,10 @@ def prime?(number)
 
 factors = Array(1..number)
   
-puts factors
+##puts factors
 factors.pop
 factors.shift
-puts factors
+##puts factors
 
 
 
@@ -20,10 +20,10 @@ non_prime_array = []
        non_prime_array.push(factor)
       end 
      end
- puts non_prime_array
+ ##puts non_prime_array
 
 
- puts non_prime_array.size == 0
+ non_prime_array.size == 0
   
    
 end
