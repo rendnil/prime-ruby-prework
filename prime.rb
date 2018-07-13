c668566857e5a328ac5435c8ae90cbc0b7ccb8c4
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-13 18:10:32 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,34 +3,34 @@
 
 def prime?(number)
 
-if number<0
-  false
-end  
+  if number<0
+    false
+  end  
+  elsif
 
-
-factors = Array(1..number)
+  factors = Array(1..number)
   
 ##puts factors
-factors.pop
-factors.shift
+  factors.pop
+  factors.shift
 ##puts factors
 
 
 
-non_prime_array = []
+  non_prime_array = []
 ##puts non_prime_array.size
    
-    factors.each do |factor|
-      if number%factor == 0
-       non_prime_array.push(factor)
-      end 
-     end
+      factors.each do |factor|
+        if number%factor == 0
+         non_prime_array.push(factor)
+        end 
+       end
  ##puts non_prime_array
 
 
  non_prime_array.size == 0
   
-   
+ end
 end
 
 
